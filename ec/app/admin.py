c0e4eca5aea80a8c927a20f92f43df662c885804
Sidratul Muntaha <smuntaha226@gmail.com>
2025-06-17 11:41:42 +0600

    Initial commit

---
+++ ec/app/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Cart, Customer ,Product
+# Register your models here.
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display= ['id','title','discounted_price','category','product_image']
+
+@admin.register(Customer)
+class CustromerModelAdmin(admin.ModelAdmin):
+    list_display= ['id','user','locality','city','state','zipcode']
+
+@admin.register(Cart)
+class CartModelAdmin(admin.ModelAdmin):
+    list_display= ['id','user','product','quantity']
---
+++ ec/app/models.py
@@ -0,0 +1,87 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+STATE_CHOICES = (
+    ('AP', 'Andhra Pradesh'),
+    ('AR', 'Arunachal Pradesh'),
+    ('AS', 'Assam'),
+    ('BR', 'Bihar'),
+    ('CG', 'Chhattisgarh'),
+    ('GA', 'Goa'),
+    ('GJ', 'Gujarat'),
+    ('HR', 'Haryana'),
+    ('HP', 'Himachal Pradesh'),
+    ('JH', 'Jharkhand'),
+    ('KA', 'Karnataka'),
+    ('KL', 'Kerala'),
+    ('MP', 'Madhya Pradesh'),
+    ('MH', 'Maharashtra'),
+    ('MN', 'Manipur'),
+    ('ML', 'Meghalaya'),
+    ('MZ', 'Mizoram'),
+    ('NL', 'Nagaland'),
+    ('OD', 'Odisha'),
+    ('PB', 'Punjab'),
+    ('RJ', 'Rajasthan'),
+    ('SK', 'Sikkim'),
+    ('TN', 'Tamil Nadu'),
+    ('TS', 'Telangana'),
+    ('TR', 'Tripura'),
+    ('UP', 'Uttar Pradesh'),
+    ('UK', 'Uttarakhand'),
+    ('WB', 'West Bengal'),
+    ('AN', 'Andaman and Nicobar Islands'),
+    ('CH', 'Chandigarh'),
+    ('DN', 'Dadra and Nagar Haveli and Daman and Diu'),
+    ('DL', 'Delhi'),
+    ('JK', 'Jammu and Kashmir'),
+    ('LA', 'Ladakh'),
+    ('LD', 'Lakshadweep'),
+    ('PY', 'Puducherry'),
+)
+
+CATEGORY_CHOICES = (
+    ('CR', 'Curd'),
+    ('ML', 'Milk'),
+    ('LS', 'Lassi'),
+    ('MS', 'Milkshake'),
+    ('PN', 'Paneer'),
+    ('GH', 'Ghee'),
+    ('CZ', 'Cheese'),
+    ('IC', 'Ice-creams'),
+)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default=' ')
+    prodapp = models.TextField(default=' ')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='product')  # fixed typo 'produt'
+
+    def __str__(self):
+        return self.title
+
+class Customer(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state=models.CharField(choices=STATE_CHOICES,max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Cart(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1)
+
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
